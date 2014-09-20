89f033f9aad5298bf11d8ba84a01ee5ef1d3efe8
mcsauder <mcsauder@greypointcorp.com>
2014-09-20 14:26:33 -0600

    Updated test, commented out the differences section.

--- tests/testLaurieGautschi.cpp
+++ tests/testLaurieGautschi.cpp
@@ -4,20 +4,22 @@
 
 int compare_codes(void)
 {
-    typedef double RealType;
-    //typedef mpfr::mpreal RealType;
+    //typedef double RealType;
+    typedef mpfr::mpreal RealType;
     //RealType::set_default_prec(128);
-    //RealType::set_default_prec(256);
+    RealType::set_default_prec(256);
 
     std::cout<<std::endl<<"MS Laurie Gautschi"<<std::endl;
     Eigen::Array<RealType,Dynamic,2> ans;
     int n = 10;
+    int outputIntegers = 35;
     ans = Kronrod::multiPrecisionKronrod<RealType>(n);
 
     std::cout<<std::fixed;
     for(int i=0;i<ans.rows();++i)
     {
-        std::cout<<std::setprecision(15)<<ans(i,0)<<"\t"<<ans(i,1)<<std::endl;
+        std::cout << std::setprecision(outputIntegers) << ans(i,0)
+		  << "\t" << ans(i,1) << std::endl;
     }
 
     std::cout<<"\nSTB Laurie Gautschi"<<std::endl;
@@ -34,11 +36,16 @@ int compare_codes(void)
 
     for(IndexType i=0;i<x.rows();++i)
     {
-        std::cout<<std::setprecision(15)<<x(i)<<"\t"<<w(i)<<std::endl;
+        std::cout << std::setprecision(outputIntegers) << x(i) 
+		  << "\t" << w(i) << std::endl;
     }
 
-    Eigen::Array<RealType, Dynamic, 1> xGKPosAndNeg = Eigen::Array<RealType, Dynamic, 1>::Zero(x.rows());
-    Eigen::Array<RealType, Dynamic, 1> wGKPosAndNeg = Eigen::Array<RealType, Dynamic, 1>::Zero(x.rows());;
+    Eigen::Array<RealType, Dynamic, 1> xGKPosAndNeg 
+	= Eigen::Array<RealType, Dynamic, 1>::Zero(x.rows());
+    
+    Eigen::Array<RealType, Dynamic, 1> wGKPosAndNeg 
+	= Eigen::Array<RealType, Dynamic, 1>::Zero(x.rows());;
+    
     Eigen::Array<RealType, Dynamic, 1> xGK;
     Eigen::Array<RealType, Dynamic, 1> wGK;
     Eigen::Array<RealType, Dynamic, 1> wG;
@@ -60,28 +67,36 @@ int compare_codes(void)
     std::cout << "\nMS Piessens" << std::endl;
     for(IndexType i=0;i<xGKPosAndNeg.rows();++i)
     {
-        std::cout<<std::setprecision(15)<<xGKPosAndNeg(i)<<"\t"<<wGKPosAndNeg(i)<<std::endl;
+        std::cout << std::setprecision(outputIntegers) << xGKPosAndNeg(i) 
+		  << "\t"<<wGKPosAndNeg(i) << std::endl;
     }
+
+    std::cout<<std::endl;
+
+/*------------------------Output of Differences Between Approaches-------------------------------//
 
     std::cout<<"\nSolution Differences: MS - STB Laurie Gautschi"<<std::endl;
     for(int i=0;i<ans.rows();++i)
     {
-        std::cout<<std::setprecision(15)<<ans(i,0) - x(i)<<"\t"<<ans(i,1) - w(i)<<std::endl;
+        std::cout << std::setprecision(15) << ans(i,0) - x(i) 
+		  << "\t" << ans(i,1) - w(i) << std::endl;
     }
 
     std::cout<<"\nSolution Differences: MS Laurie Gautschi - MS Piessens"<<std::endl;
     for(int i=0;i<ans.rows();++i)
     {
-        std::cout<<std::setprecision(15)<<ans(i,0) - xGKPosAndNeg(i)<<"\t"<<ans(i,1) - wGKPosAndNeg(i)<<std::endl;
+        std::cout << std::setprecision(15) << ans(i,0) - xGKPosAndNeg(i)
+		  << "\t" << ans(i,1) - wGKPosAndNeg(i) << std::endl;
     }
 
     std::cout<<"\nSolution Differences: STB Laurie Gautschi - MS Piessens"<<std::endl;
     for(int i=0;i<x.rows();++i)
     {
-        std::cout<<std::setprecision(15)<< x(i)-xGKPosAndNeg(i)<<"\t"<<w(i)-wGKPosAndNeg(i)<<std::endl;
+        std::cout  << std::setprecision(15) << x(i) - xGKPosAndNeg(i) 
+		   << "\t" << w(i) - wGKPosAndNeg(i) << std::endl;
     }
 
-    std::cout<<std::endl;
+//-----------------------End Output of Differences Between Approaches----------------------------*/
 
     return EXIT_SUCCESS;
 }
