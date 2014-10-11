c98c332a52483c231dd11405601112007ce660e7
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2014-10-11 23:54:41 +0100

    example tested with release flags

--- examples/Integration.cpp
+++ examples/Integration.cpp
@@ -5,9 +5,10 @@
 // We may need to put all the codes some common directory
 #include <NIHeaders.hpp>
 #include <iostream>
+#include <iomanip>
 
 /* We consider the example from
-http://www.gnu.org/software/gsl/manual/html_node/Numerical-integration-examples.html#Numerical-integration-examples
+http://www.gnu.org/software/gsl/manual/html_node/Numerical-integration-examples.html
 
 \int_0^1 x^{-1/2} log(x) dx = -4
 
@@ -21,10 +22,14 @@ class IntegrandExampleFunctor
 public:
     IntegrandExampleFunctor(const Scalar alpha)
     :m_alpha(alpha)
-
-    Scalar operator()(const Scalar param) const
     {
-        log(m_alpha*x) / sqrt(x);
+        assert(alpha>0);
+    }
+
+    Scalar operator()(const Scalar x) const
+    {
+        assert(x>0);
+        return log(m_alpha*x) / sqrt(x);
     }
 
     void setAlpha(const Scalar alpha)
@@ -37,14 +42,36 @@ private:
 
 int main(void)
 {
+    //define the sclar
     typedef double Scalar;
 
     //define the functor
     Scalar alpha=1.;
-    IntegrandExampleFunctor<Scalar> itfctr(alpha);
+    IntegrandExampleFunctor<Scalar> inFctr(alpha);
+
+    //define the integrator
+    Eigen::Integrator<Scalar> eigIntgtor(200);
 
     //define a quadrature rule
     Eigen::Integrator<Scalar>::QuadratureRule quadratureRule = Eigen::Integrator<Scalar>::GaussKronrod61;
+
+    //define the desired absolute and relative errors
+    Scalar desAbsErr = Scalar(0.);
+    Scalar desRelErr = Eigen::NumTraits<Scalar>::epsilon() * 50.;
+
+    //integrate
+    Scalar result = eigIntgtor.quadratureAdaptive(inFctr, Scalar(0.),Scalar(1.), desAbsErr, desRelErr, quadratureRule);
+
+    //expected result
+    Scalar expected = Scalar(-4.);
+
+    //print output
+    size_t outputPrecision  = 18;
+    std::cout<<std::fixed;
+    std::cout<<"result          = "<<std::setprecision(outputPrecision)<<result<<std::endl;
+    std::cout<<"exact result    = "<<std::setprecision(outputPrecision)<<expected<<std::endl;
+    std::cout<<"actual error    = "<<std::setprecision(outputPrecision)<<(expected-result)<<std::endl;
+
 
     return 0;
 }
