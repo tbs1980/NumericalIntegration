b03ea795653dded3f1212f312296c2d0ad893eb9
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-02-07 09:53:01 +0000

    Monegato mathod added. Not working yet. there is a segfault

--- GaussKronrodNodesWeights.h
+++ GaussKronrodNodesWeights.h
@@ -478,7 +478,7 @@ namespace Kronrod {
         *    The arrays abcsGaussKronrod, weightGaussKronrod and weightGauss contain the
         *    positive abscissae in decreasing order, and the weights of each abscissa in
         *    the Gauss-Kronrod and Gauss rules, respectively.
-        *    
+        *
         *    Ported to C++/Eigen and templated for multiprecision by Mark Sauder,
         *    Sreekumar Thaithara Balan, Matt Beall, and R. Jeff Jenkins - September 2014.
         *
@@ -783,7 +783,7 @@ namespace Kronrod {
 
                 // If nNodes <= 1, initialize p2 and pd2 to avoid problems calculating delta.
                 if (nNodes <= 1)
-                {   
+                {
                     using std::abs;
                     if (machineEpsilon() < abs(abscGaussKronrod))
                     {
@@ -886,6 +886,284 @@ namespace Kronrod {
         }
     };
 //--------------------------------------End Piessens Class---------------------------------------//
+
+
+//--------------------------------------Montegato Method---------------------------------------//
+
+    /**
+     * \brief A class for computign the Gauss/Kronrod weights.
+     *
+     * \tparam _Scalar floating point type
+     */
+    template<typename _Scalar>
+    class Monegato
+    {
+    public:
+        typedef _Scalar Scalar;
+        typedef Eigen::Array<Scalar, Eigen::Dynamic, 1> ScalarArrayType;
+        typedef typename ScalarArrayType::Index Index;
+
+
+
+        static Scalar legendre_err(int const n, Scalar const x, Scalar & err)
+        {
+            using std::abs;
+            if (n == 0)
+            {
+                err = Scalar(0);
+                return Scalar(1);
+            }
+            else if (n == 1)
+            {
+                err = Scalar(0);
+                return x;
+            }
+
+            // below Sree modified this to avoid -Wmaybe-uninitialized
+            Scalar P0 = Scalar(1), P1 = x, P2=x;
+            Scalar E0 = Eigen::NumTraits<Scalar>::epsilon();
+            Scalar E1 = abs(x) * Eigen::NumTraits<Scalar>::epsilon();
+            for (int k = 1; k < n; ++k)
+            {
+                P2 = ((2*k + 1) * x * P1 - k * P0) / (k + 1);
+                err = ((2*k + 1) * abs(x) * E1 + k * E0) / (2*(k + 1));
+                P0 = P1; P1 = P2;
+                E0 = E1; E1 = err;
+            }
+            return P2;
+        }
+
+        static Scalar legendre_deriv(int const n, Scalar const x)
+        {
+            if (n == 0)
+            {
+                return Scalar(0);
+            }
+            else if (n == 1)
+            {
+                return Scalar(1);
+            }
+
+            Scalar P0 = Scalar(1), P1 = x, P2;
+            // below Sree modified this to avoid -Wmaybe-uninitialized
+            Scalar dP0 = Scalar(0), dP1 = Scalar(1), dP2=P1;
+            for (int k = 1; k < n; ++k)
+            {
+                P2 = ((2*k + 1) * x * P1 - k * P0) / (k + Scalar(1));
+                dP2 = (2*k + 1) * P1 + dP0;
+                P0 = P1;
+                P1 = P2;
+                dP0 = dP1;
+                dP1 = dP2;
+            }
+            return dP2;
+        }
+
+        static Scalar chebyshev_series_deriv(Scalar const x,int const n_,
+            ScalarArrayType const & coefs)
+        {
+            Scalar d1(0), d2(0);
+            Scalar y2 = 2 * x; // linear term for Clenshaw recursion
+
+            for (int k = n_; k >= 2; --k)
+            {
+                Scalar temp = d1;
+                d1 = y2 * d1 - d2 + k * coefs(k);
+                d2 = temp;
+            }
+
+            return y2 * d1 - d2 + coefs(1);
+        }
+
+        static Scalar chebyshev_series(Scalar const x, int const n_,
+            ScalarArrayType const & coefs, Scalar& err)
+        {
+            using std::abs;
+            Scalar d1(0), d2(0);
+            Scalar absc = abs(coefs(0)); // final term for truncation error
+            Scalar y2 = 2 * x; // linear term for Clenshaw recursion
+
+            for (int k = n_; k >= 1; --k)
+            {
+                Scalar temp = d1;
+                d1 = y2 * d1 - d2 + coefs(k);
+                d2 = temp;
+                absc += abs(coefs(k));
+            }
+
+            err = absc * Eigen::NumTraits<Scalar>::epsilon();
+            return x * d1 - d2 + coefs(0)/2.;
+        }
+
+
+        static void legendre_zeros(int const m_,ScalarArrayType & zeros)
+        {
+            using std::abs;
+            ScalarArrayType temp = ScalarArrayType::Zero(m_+1);
+            zeros(0) = Scalar(-1);
+            zeros(1) = Scalar(1);
+            Scalar delta, epsilon;
+
+            for (int k = 1; k <= m_; ++k)
+            {
+                // loop to locate zeros of P_k interlacing z_0,...,z_k
+                for (int j = 0; j < k; ++j)
+                {
+                    // Newton's method for P_k :
+                    // initialize solver at midpoint of (z_j, z_{j+1})
+                    delta = 1;
+                    Scalar x_j = (zeros(j) + zeros(j+1)) / 2.;
+                    Scalar P_k = legendre_err(k, x_j, epsilon);
+                    while (abs(P_k) > epsilon &&
+                        abs(delta) > Eigen::NumTraits<Scalar>::epsilon())
+                    {
+                        delta = P_k / legendre_deriv(k, x_j);
+                        x_j -= delta;
+                        P_k = legendre_err(k, x_j, epsilon);
+                    }
+                    temp(j) = x_j;
+                }
+
+                // copy roots tmp_0,...,tmp_{k-1} to z_1,...z_k:
+                zeros(k+1) = zeros(k);
+                for (int j = 0; j < k; ++j)
+                {
+                    zeros(j+1) = temp(j);
+                }
+            }
+
+        }
+
+        static void chebyshev_coefs(int const m_,ScalarArrayType & coefs)
+        {
+            size_t ell = (m_ + 1)/2;
+            ScalarArrayType alpha = ScalarArrayType::Zero(ell+1);
+            ScalarArrayType f = ScalarArrayType::Zero(ell+1);
+
+            // Care must be exercised in initalizing the constants in the definitions.
+            // Compilers interpret expressions like "(2*k + 1.0)/(k + 2.0)" as floating
+            // point precision, before casting to Real.
+
+            f(1) = Scalar(m_+1)/Scalar(2*m_ + 3);
+            alpha(0) = Scalar(1); // coefficient of T_{m+1}
+            alpha(1) = -f(1);
+
+            for (int k = 2; k <= ell; ++k)
+            {
+                f(k) = f(k-1) * (2*k - 1) * (m_ + k) / (k * (2*m_ + 2*k + 1));
+                alpha(k) = -f(k);
+                for (int i = 1; i < k; ++i)
+                {
+                    alpha(k) -= f(i) * alpha(k-i);
+                }
+            }
+
+            for (int k = 0; k <= ell; ++k)
+            {
+                coefs(m_ + 1 - 2*k) = alpha(k);
+                if (m_  >= 2*k)
+                {
+                    coefs(m_ - 2*k) = Scalar(0);
+                }
+            }
+        }
+
+        static void gauss_kronrod_abscissae(int const n_,int const m_,
+            ScalarArrayType const & zeros, ScalarArrayType const & coefs,
+            ScalarArrayType & xgk_)
+        {
+            //
+            // now from the function gauss_kronrod_abscissae
+            //
+
+            Scalar delta, epsilon;
+
+            for (int k = 0; k < n_ / 2; ++k)
+            {
+                delta = 1;
+                // Newton's method for E_{n+1} :
+                Scalar x_k = (zeros(m_-k) + zeros(m_+1-k))/Scalar(2);
+                Scalar E = chebyshev_series(x_k,n_,coefs, epsilon);
+                while (abs(E) > epsilon &&
+                    abs(delta) > Eigen::NumTraits<Scalar>::epsilon() )
+                {
+                    delta = E / chebyshev_series_deriv(x_k,n_,coefs);
+                    x_k -= delta;
+                    E = chebyshev_series(x_k,n_,coefs, epsilon);
+                }
+                xgk_(2*k) = x_k;
+                // copy adjacent Legendre-zero into the array:
+                if (2*k+1 < n_)
+                {
+                    xgk_(2*k+1) = zeros(m_-k);
+                }
+            }
+        }
+
+        static void gauss_kronrod_weights(int const & n_,int const m_,
+            ScalarArrayType const & coefs, ScalarArrayType const& xgk_,
+            ScalarArrayType & wg_, ScalarArrayType & wgk_ )
+        {
+            Scalar err;
+
+            // Gauss-Legendre weights:
+            for(int k=0; k < n_ /2; ++k)
+            {
+                Scalar x = xgk_(2*k + 1);
+                wg_(k) = ( Scalar(-2)
+                    / ((m_ + 1) * legendre_deriv(m_, x) * legendre_err(m_+1, x, err)) );
+            }
+
+            // The ratio of leading coefficients of P_n and T_{n+1} is computed
+            // from the recursive formulae for the respective polynomials.
+            Scalar F_m = Scalar(2) / Scalar(2*m_ + 1);
+            for (int k = 1; k <= m_; ++k)
+            {
+                F_m *= (Scalar(2*k) / Scalar(2*k - 1));
+            }
+
+            // Gauss-Kronrod weights:
+
+            for (size_t k = 0; k < n_; ++k)
+            {
+                Scalar x = xgk_(k);
+                if (k % 2 == 0)
+                {
+                    wgk_(k) = F_m / (legendre_err(m_, x, err) * chebyshev_series_deriv(x,n_,coefs));
+                }
+                else
+                {
+                    wgk_(k) = (wg_(k/2) +
+                        F_m / (legendre_deriv(m_, x) * chebyshev_series(x,n_,coefs, err)));
+                }
+            }
+
+        }
+
+        static void computeAbscissaeAndWeights(unsigned int nNodes,
+            ScalarArrayType & xgk_,ScalarArrayType & wgk_,
+            ScalarArrayType & xk_,ScalarArrayType & wg_)
+        {
+            const Index m_ = 2*nNodes;
+            const Index n_ = m_ + 1;
+
+            /*
+            ScalarArrayType xgk_ = ScalarArrayType::Zero(n_);//2*nNodes+1
+            ScalarArrayType wgk_ = ScalarArrayType::Zero(n_);//2*nNodes+1
+            ScalarArrayType xk_ = ScalarArrayType::Zero(n_/2);//2*nNodes
+            ScalarArrayType wg_ = ScalarArrayType::Zero(n_/2);//2*nNodes
+            */
+
+            // initialise the coefficients to zero
+            ScalarArrayType coefs = ScalarArrayType::Zero(n_+1);
+            ScalarArrayType zeros = ScalarArrayType::Zero(m_+2);
+
+            legendre_zeros(m_, zeros);
+            chebyshev_coefs(m_, coefs);
+            gauss_kronrod_abscissae(n_, m_, zeros,coefs, xgk_);
+            //gauss_kronrod_weights(n_, m_,coefs, xgk_, wg_, wgk_ );
+        }
+    };
 }
 
 #endif //NI_KRONRODLAURIEGAUTSCHI_H
--- test/testNodesAndWeightsByCompare.cpp
+++ test/testNodesAndWeightsByCompare.cpp
@@ -14,10 +14,10 @@
 int compare_codes_unified_interface(void)
 {
     //typedef float Scalar;
-    //typedef double Scalar;
+    typedef double Scalar;
     //typedef long double Scalar;
-    typedef mpfr::mpreal Scalar;
-    Scalar::set_default_prec(320); //128, 320, 384, 448 gives an error Newton-Raphson iterative abscissae solver failed.
+    //typedef mpfr::mpreal Scalar;
+    //Scalar::set_default_prec(320); //128, 320, 384, 448 gives an error Newton-Raphson iterative abscissae solver failed.
     //256,288,320,352,384,416,448
     Eigen::Array<Scalar, Eigen::Dynamic, 1> xGKLaurieGautschi;
     Eigen::Array<Scalar, Eigen::Dynamic, 1> wGKLaurieGautschi;
@@ -26,9 +26,10 @@ int compare_codes_unified_interface(void)
 
     typedef Kronrod::LaurieGautschi<Scalar> LaurieGautschiPolicy;
     typedef Kronrod::Piessens<Scalar> PiessensPolicy;
+    typedef Kronrod::Monegato<Scalar> MonegatoPolicy;
 
     typedef LaurieGautschiPolicy::IndexType IndexType;
-    
+
     const unsigned int N = 100;
     const int outputIntegers = 80; //beyond 67 integers methods disagree for precision=256.
 
@@ -67,6 +68,37 @@ int compare_codes_unified_interface(void)
     PiessensPolicy::computeAbscissaeAndWeights(N,xGKPiessens,wGKPiessens,xGPiessens,wGPiessens);
 
     fout.open("Piessens320.dat");
+
+    fout << "Kronrod Nodes and Weights for N = " << N << std::endl;
+    fout << "\nKronrod Nodes\n";
+    fout << std::fixed;
+    for(IndexType i = 0; i < xGKPiessens.rows(); ++i)
+    {
+        fout << std::setprecision(outputIntegers) << xGKPiessens(i) << ",\n";
+    }
+
+    fout << "\nKronrod Weights\n";
+    for(IndexType i = 0; i < wGKPiessens.rows(); ++i)
+    {
+        fout << std::setprecision(outputIntegers) << wGKPiessens(i) << ",\n";
+    }
+
+    fout << "\nGauss Weights\n";
+    for(IndexType i = 0; i < wGPiessens.rows(); ++i)
+    {
+        fout << std::setprecision(outputIntegers) << wGPiessens(i) << ",\n";
+    }
+
+    fout.close();
+
+    Eigen::Array<Scalar, Eigen::Dynamic, 1> xGKMonegato;
+    Eigen::Array<Scalar, Eigen::Dynamic, 1> wGKMonegato;
+    Eigen::Array<Scalar, Eigen::Dynamic, 1> xGMonegato;
+    Eigen::Array<Scalar, Eigen::Dynamic, 1> wGMonegato;
+
+    MonegatoPolicy::computeAbscissaeAndWeights(N,xGKMonegato,wGKMonegato,xGMonegato,wGMonegato);
+
+    fout.open("Monegato320.dat");
 
     fout << "Kronrod Nodes and Weights for N = " << N << std::endl;
     fout << "\nKronrod Nodes\n";
