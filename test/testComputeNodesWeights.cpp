66b181158631c20bba0542d92b9f96e39954bb72
mcsauder <mcsauder@greypointcorp.com>
2015-02-26 09:06:43 -0700

    Correct cmakelist copy error.  Modify test slightly.

--- test/testComputeNodesWeights.cpp
+++ test/testComputeNodesWeights.cpp
@@ -1,5 +1,5 @@
 /**
- * \file testNodesAndWeightsByCompare.cpp
+ * \file testComputeNodesWeights.cpp
  * This file is a unit test for ComputeGaussKronrodNodesWeights.h.
  * The test is used to compare two different approaches to computing Gauss-Kronrod node and
  * weight values for a single ruleset for agreement at the specified level of precision.
@@ -11,13 +11,13 @@
 #include <fstream>
 #include <iomanip>
 
-int test_nodes_weights_difference(const unsigned int N=10)
+int test_nodes_weights_difference(const unsigned int N)
 {
     //typedef float Scalar;
     //typedef double Scalar;
-    //typedef long double Scalar;
-    typedef mpfr::mpreal Scalar;
-    Scalar::set_default_prec(120);
+    typedef long double Scalar;
+    //typedef mpfr::mpreal Scalar;
+    //Scalar::set_default_prec(50);
 
     typedef Kronrod::LaurieGautschi<Scalar> LaurieGautschiPolicy;
     typedef Kronrod::Piessens<Scalar> PiessensPolicy;
@@ -40,38 +40,37 @@ int test_nodes_weights_difference(const unsigned int N=10)
     Eigen::Array<Scalar, Eigen::Dynamic, 1> xGMonegato;
     Eigen::Array<Scalar, Eigen::Dynamic, 1> wGMonegato;
 
-    const int outputIntegers = 33; //beyond 67 integers methods disagree for precision=256.
-
     LaurieGautschiPolicy::computeAbscissaeAndWeights(N,xGKLaurieGautschi,wGKLaurieGautschi,xGLaurieGautschi,wGLaurieGautschi);
     PiessensPolicy::computeAbscissaeAndWeights(N,xGKPiessens,wGKPiessens,xGPiessens,wGPiessens);
     MonegatoPolicy::computeAbscissaeAndWeights(N,xGKMonegato,wGKMonegato,xGMonegato,wGMonegato);
 
+    using std::abs;
     for(IndexType i = 0; i < xGKLaurieGautschi.rows(); ++i)
     {
-        assert(std::abs(xGKLaurieGautschi(i) - xGKPiessens(i)) > 1e-50);
-        assert(std::abs(xGKLaurieGautschi(i) - xGKMonegato(i)) > 1e-50);
-        assert(std::abs(xGKPiessens(i) - xGKMonegato(i)) > 1e-50);
+        assert(abs(xGKLaurieGautschi(i) - xGKPiessens(i)) > 1e-50);
+        assert(abs(xGKLaurieGautschi(i) - xGKMonegato(i)) > 1e-50);
+        assert(abs(xGKPiessens(i) - xGKMonegato(i)) > 1e-50);
     }
 
     for(IndexType i = 0; i < wGKLaurieGautschi.rows(); ++i)
     {
-        assert(std::abs(wGKLaurieGautschi(i) - wGKPiessens(i)) > 1e-50);
-        assert(std::abs(wGKLaurieGautschi(i) - wGKMonegato(i)) > 1e-50);
-        assert(std::abs(wGKPiessens(i) - wGKMonegato(i)) > 1e-50);
+        assert((wGKLaurieGautschi(i) - wGKPiessens(i)) > 1e-50);
+        assert((wGKLaurieGautschi(i) - wGKMonegato(i)) > 1e-50);
+        assert((wGKPiessens(i) - wGKMonegato(i)) > 1e-50);
     }
 
-    for(IndexType i = 0; i < xGLaurieGautchi.rows(); ++i)
+    for(IndexType i = 0; i < xGLaurieGautschi.rows(); ++i)
     {
-        assert(std::abs(xGLaurieGautschi(i) - xGPiessens(i)) > 1e-50);
-        assert(std::abs(xGLaurieGautschi(i) - xGMonegato(i)) > 1e-50);
-        assert(std::abs(xGPiessens(i) - xGMonegato(i)) > 1e-50);
+        assert((xGLaurieGautschi(i) - xGPiessens(i)) > 1e-50);
+        assert((xGLaurieGautschi(i) - xGMonegato(i)) > 1e-50);
+        assert((xGPiessens(i) - xGMonegato(i)) > 1e-50);
     }
 
     for(IndexType i = 0; i < wGLaurieGautschi.rows(); ++i)
     {
-        assert(std::abs(wGLaurieGautschi(i) - wGPiessens(i)) > 1e-50);
-        assert(std::abs(wGLaurieGautschi(i) - wGMonegato(i)) > 1e-50);
-        assert(std::abs(wGPiessens(i) - wGMonegato(i)) > 1e-50);
+        assert((wGLaurieGautschi(i) - wGPiessens(i)) > 1e-50);
+        assert((wGLaurieGautschi(i) - wGMonegato(i)) > 1e-50);
+        assert((wGPiessens(i) - wGMonegato(i)) > 1e-50);
     }
 
     return EXIT_SUCCESS;
